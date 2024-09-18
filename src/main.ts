ca4ef6f791e2502a39ca443933a0e238bd100afa
Dairo Barrios <dairo.barrios.ramos@gmail.com>
2024-09-18 13:37:45 -0500

    validations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   // Configurar títulos de documentación
   const options = new DocumentBuilder()
     .setTitle('API RESFULL - DAIRO')
--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,23 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateUsuarioDto {
   @ApiProperty({
     example: 'Dairo Barrios Ramos',
   })
+  @IsString()
   readonly nombre: string;
 
   @ApiProperty({
     example: 'CC',
   })
+  @IsString()
   readonly tipo_documento: string;
 
   @ApiProperty({
     example: '123456789',
   })
+  @IsNumber()
   readonly documento: number;
 
   @ApiProperty({
     example: 'Masculino',
   })
+  @IsString()
   readonly sexo: string;
 }
